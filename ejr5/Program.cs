49606a3e9a91556e970efd0c1267714b061cfb5e
datasoftuy <andres.esteves.aharonian@gmail.com>
2017-05-09 11:46:53 -0300

    finalizado

---
+++ ejr5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ejr5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int edad;
+            string nombre;
+
+
+            Console.Write("Ingrese nombre: ");
+            nombre = Console.ReadLine();
+
+
+
+
+            Console.Write("Ingrese edad: ");
+            edad = Convert.ToInt32(Console.ReadLine());
+
+            if (edad > 18)
+
+            {
+                Console.Write("Nombre: "+ nombre);
+                Console.Write("Edad  : "+edad);
+                
+            }
+
+            Console.ReadLine();
+        
+        }
+    }
+}
